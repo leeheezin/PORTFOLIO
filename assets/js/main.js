393faa00defd1f7987abb98e35fe5f00d1b6825a
leeheezin <102892982+leeheezin@users.noreply.github.com>
2022-04-28 16:58:25 +0900

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,76 @@
+//SupahScroll
+class SupahScroll {
+  constructor(options) {
+    this.opt = options || {};
+    this.el = this.opt.el ? this.opt.el : ".supah_scroll";
+    this.speed = this.opt.speed ? this.opt.speed : 0.1;
+    this.init();
+  }
+
+  init() {
+    this.scrollY = 0;
+    this.supahScroll = document.querySelectorAll(this.el)[0];
+    this.supahScroll.classList.add("supah_scroll");
+    this.events();
+    this.update();
+    this.animate();
+  }
+
+  update() {
+    if (this.supahScroll === null) return;
+    document.body.style.height = `${
+      this.supahScroll.getBoundingClientRect().height
+    }px`;
+  }
+
+  pause() {
+    document.body.style.overflow = "hidden";
+    cancelAnimationFrame(this.raf);
+  }
+
+  play() {
+    document.body.style.overflow = "inherit";
+    this.raf = requestAnimationFrame(this.animate.bind(this));
+  }
+
+  destroy() {
+    this.supahScroll.classList.remove("supah_scroll");
+    this.supahScroll.style.transform = "none";
+    document.body.style.overflow = "inherit";
+    window.removeEventListener("resize", this.update);
+    cancelAnimationFrame(this.raf);
+    delete this.supahScroll;
+  }
+
+  animate() {
+    this.scrollY += (window.scrollY - this.scrollY) * this.speed;
+    this.supahScroll.style.transform = `translate3d(0,${-this.scrollY}px,0)`;
+    this.raf = requestAnimationFrame(this.animate.bind(this));
+  }
+
+  scrollTo(y) {
+    window.scrollTo(0, y);
+  }
+
+  staticScrollTo(y) {
+    cancelAnimationFrame(this.raf);
+    this.scrollY = y;
+    window.scrollTo(0, y);
+    this.supahScroll.style.transform = `translate3d(0,${-y}px,0)`;
+    this.play();
+  }
+
+  events() {
+    window.addEventListener("load", this.update.bind(this));
+    window.addEventListener("resize", this.update.bind(this));
+  }
+}
+//Initialize
+const supahscroll = new SupahScroll({
+  el: "#wrapper",
+  speed: 0.1
+});
+
 //scroll
  $(window).scroll(function(){
       let scrollTop = $(window).scrollTop(); //scrolltop(): 브라우저 위치값
@@ -15,7 +88,25 @@ const headerHeight = header.getBoundingClientRect().height;
            header.setAttribute("style", "background: #1458E4;");
       }
     });
+//header sidebar open - close
+// $(function() {
+// $("#nav").on("click",function(){
+//   $(".side_inner").toggle('open');
+// });
+// }); 	
+// $(function(){
+//   var sideBar = $(".sidebar");
+//   var menuBtn = $(".nav_button");
+//   var menuClose = $(".nav_close");
 
+// 	menuBtn.click(function(){
+// 		sideBar.toggleClass("show2");
+// 	});
+
+// 	menuClose.click(function(){
+//     sideBar.removeClass("show2");
+// 	});
+// });
   
   
 
@@ -48,13 +139,13 @@ const headerHeight = header.getBoundingClientRect().height;
   setTimeout(() => {
     gsap.to(".left_txt .left_txt1", {duration: 0.6, width: "100%"});
     gsap.to(".left_txt .left_txt2", {duration: 0.6, width: "100%", delay: 0.2});
-    gsap.to(".left_txt .left_txt3", {duration: 0.6, width: "100%", delay: 0.4});
-    gsap.to(".left_txt .left_txt4", {duration: 0.6, width: "100%", delay: 0.6});
+    gsap.to(".left_txt .left_txt3", {duration: 0.6, width: "100%", delay: 0.3});
+    gsap.to(".left_txt .left_txt4", {duration: 0.6, width: "100%", delay: 0.4});
 
-    gsap.to(".left_txt .left_txt1 .active", {duration: 0.4, stagger: 0.05, opacity: 1, y: 0, delay: 0.4})
-    gsap.to(".left_txt .left_txt2 .active", {duration: 0.4, stagger: 0.05, opacity: 1, y: 0, delay: 0.6})
-    gsap.to(".left_txt .left_txt3 .active", {duration: 0.4, stagger: 0.05, opacity: 1, y: 0, delay: 0.8})
-    gsap.to(".left_txt .left_txt4 .active", {duration: 0.4, stagger: 0.05, opacity: 1, y: 0, delay: 1.0})
+    gsap.to(".left_txt .left_txt1 .active", {duration: 0.4, stagger: 0.05, opacity: 1, y: 0, delay: 0.2})
+    gsap.to(".left_txt .left_txt2 .active", {duration: 0.4, stagger: 0.05, opacity: 1, y: 0, delay: 0.4})
+    gsap.to(".left_txt .left_txt3 .active", {duration: 0.4, stagger: 0.05, opacity: 1, y: 0, delay: 0.6})
+    gsap.to(".left_txt .left_txt4 .active", {duration: 0.4, stagger: 0.05, opacity: 1, y: 0, delay: 0.8})
 }, 2000)
 //   Splitting();
 
@@ -82,12 +173,21 @@ const headerHeight = header.getBoundingClientRect().height;
 // });
 
 
-//section3 text
+//section 나타내기 효과 / img + txt 이질감
 function scrollProgress(){
-  let scrollTop = (document.documentElement.scrollTop)
-  // .prof_intro 
-  if( scrollTop > document.getElementById("section3").offsetTop - $(window).height()/2){
-      document.getElementById("section3").classList.add("show");
-  }
+  let scrollTop = (document.documentElement.scrollTop || window.scrollY || window.pageYOffset) + window.innerHeight;
+  document.querySelectorAll(".cont_item").forEach(item =>{
+    scrollTop > item.offsetTop ? item.classList.add("show") : item.classList.remove("show");
+})
+document.querySelectorAll("#section6").forEach(item =>{
+  let offset1 = (scrollTop - item.offsetTop) * 0.1;
+  let offset2 = (scrollTop - item.offsetTop) * 0.05;
+  
+  item.querySelector(".js_img").style.transform = "translateX("+ offset1 +"px)";
+  item.querySelector(".js_txt h2").style.transform = "translateX("+ -offset2 +"px)";
+  item.querySelector(".js_txt p").style.transform = "translateX("+ offset2 +"px)";
+});
 }
 window.addEventListener("scroll", scrollProgress);
+
+// section6 h2 움직이기
