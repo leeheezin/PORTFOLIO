d663a7920f333e5152c1fc46d1eb347825e4bab0
leeheezin <102892982+leeheezin@users.noreply.github.com>
2022-05-16 16:33:15 +0900

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,76 +1,3 @@
-// //SupahScroll
-// class SupahScroll {
-//   constructor(options) {
-//     this.opt = options || {};
-//     this.el = this.opt.el ? this.opt.el : ".supah_scroll";
-//     this.speed = this.opt.speed ? this.opt.speed : 0.1;
-//     this.init();
-//   }
-
-//   init() {
-//     this.scrollY = 0;
-//     this.supahScroll = document.querySelectorAll(this.el)[0];
-//     this.supahScroll.classList.add("supah_scroll");
-//     this.events();
-//     this.update();
-//     this.animate();
-//   }
-
-//   update() {
-//     if (this.supahScroll === null) return;
-//     document.body.style.height = `${
-//       this.supahScroll.getBoundingClientRect().height
-//     }px`;
-//   }
-
-//   pause() {
-//     document.body.style.overflow = "hidden";
-//     cancelAnimationFrame(this.raf);
-//   }
-
-//   play() {
-//     document.body.style.overflow = "inherit";
-//     this.raf = requestAnimationFrame(this.animate.bind(this));
-//   }
-
-//   destroy() {
-//     this.supahScroll.classList.remove("supah_scroll");
-//     this.supahScroll.style.transform = "none";
-//     document.body.style.overflow = "inherit";
-//     window.removeEventListener("resize", this.update);
-//     cancelAnimationFrame(this.raf);
-//     delete this.supahScroll;
-//   }
-
-//   animate() {
-//     this.scrollY += (window.scrollY - this.scrollY) * this.speed;
-//     this.supahScroll.style.transform = `translate3d(0,${-this.scrollY}px,0)`;
-//     this.raf = requestAnimationFrame(this.animate.bind(this));
-//   }
-
-//   scrollTo(y) {
-//     window.scrollTo(0, y);
-//   }
-
-//   staticScrollTo(y) {
-//     cancelAnimationFrame(this.raf);
-//     this.scrollY = y;
-//     window.scrollTo(0, y);
-//     this.supahScroll.style.transform = `translate3d(0,${-y}px,0)`;
-//     this.play();
-//   }
-
-//   events() {
-//     window.addEventListener("load", this.update.bind(this));
-//     window.addEventListener("resize", this.update.bind(this));
-//   }
-// }
-// //Initialize
-// const supahscroll = new SupahScroll({
-//   el: "#wrapper",
-//   speed: 0.1,
-// });
-
 // //scroll
 // $(window).scroll(function () {
 //   let scrollTop = $(window).scrollTop(); //scrolltop(): 브라우저 위치값
@@ -212,11 +139,16 @@ gsap.set(".split > span", { opacity: 0, y: 50 });
 setTimeout(() => {
   let tl = gsap.timeline();
   tl.to("#header", { duration: 0.3, left: 0 });
-  tl.to(".sec1_inner .sec1_left", { duration: 0.4, opacity: 1, width: "63%" });
+  tl.to(".sec1_inner .sec1_left", { duration: 0.5, opacity: 1, width: "63%" });
   tl.to(".sec1_inner .sec1_right", {
-    duration: 0.2,
+    duration: 0.5,
     opacity: 1,
     width: "37%",
+  });
+  tl.to("#section2", {
+    duration: 0.2,
+    opacity: 1,
+    y: 0,
   });
   tl.to(".split > span", {
     duration: 0.5,
@@ -224,12 +156,6 @@ setTimeout(() => {
     stagger: 0.08,
     y: 0,
     ease: "expo.out",
-  });
-  tl.to(".sec2_inner", { duration: 0.6, opacity: 1, y: 0 });
-  tl.to("#section2", {
-    duration: 0.2,
-    opacity: 1,
-    y: 0,
   });
   tl.to(".sec1_inner .sec1_right .title", { opacity: 1 });
   tl.to(".sec1_inner .sec1_right .intro p", {
@@ -255,84 +181,85 @@ window.addEventListener("scroll", () => {
 
   const offset1 =
     scrollTop - document.querySelector(".intro_text h2").offsetTop;
-  gsap.to(".intro_text h2 span", { y: offset1 * 0.01 });
-  // gsap.to(".intro_text h2 em", { y: offset1 * 0.02 });
+  gsap.to(".intro_text h2 span", { y: offset1 * 0.02 });
+  gsap.to(".intro_text h2 em", { y: offset1 * 0.01 });
 
-  gsap.set(".prof_tit p", { opacity: 0, y: 100 });
+  gsap.set(".prof_tit p", { opacity: 0, x: -120 });
   gsap.set(".prof_img", { opacity: 0, y: 100 });
-  gsap.set(".prof_desc p > .desc1", { opacity: 0, y: 50 });
-  gsap.set(".prof_desc p > .desc2", { opacity: 0, y: 50 });
-  gsap.set(".prof_desc p > .desc3", { opacity: 0, y: 50 });
-  gsap.set(".prof_desc p > .desc4", { opacity: 0, y: 50 });
-  gsap.set(".prof_desc p > .desc5", { opacity: 0, y: 50 });
-  gsap.set(".prof_desc p > .desc6", { opacity: 0, y: 50 });
-  gsap.set(".prof_desc p > .desc7", { opacity: 0, y: 50 });
+  gsap.set(".prof_desc p > span", { opacity: 0, y: 50 });
 
-  setTimeout(() => {
-    let tl = gsap.timeline();
-
-    if (
-      scrollTop >
-      document.querySelector(".prof_tit").offsetTop + window.innerHeight * 1.5
-    ) {
-      tl.to(".prof_tit .tit1", {
-        duration: 0.5,
-        opacity: 1,
-        y: 0,
-      });
-      tl.to(".prof_tit .tit2", {
-        duration: 0.5,
-        opacity: 1,
-        y: 0,
-      });
-      tl.to(".prof_tit .tit3", {
-        duration: 0.5,
-        opacity: 1,
-        y: 0,
-      });
-      tl.to(".prof_tit .tit4", {
-        duration: 0.5,
-        opacity: 1,
-        y: 0,
-      });
-      tl.to(".prof_img", { duration: 0.6, opacity: 1, y: 0 });
-      tl.to(".prof_desc p > .desc1", {
-        duration: 0.5,
-        y: 0,
-        opacity: 1,
-      });
-      tl.to(".prof_desc p > .desc2", {
-        duration: 0.5,
-        y: 0,
-        opacity: 1,
-      });
-      tl.to(".prof_desc p > .desc3", {
-        duration: 0.5,
-        y: 0,
-        opacity: 1,
-      });
-      tl.to(".prof_desc p > .desc4", {
-        duration: 0.5,
-        y: 0,
-        opacity: 1,
-      });
-      tl.to(".prof_desc p > .desc5", {
-        duration: 0.5,
-        y: 0,
-        opacity: 1,
-      });
-      tl.to(".prof_desc p > .desc6", {
-        duration: 0.5,
-        y: 0,
-        opacity: 1,
-      });
-      tl.to(".prof_desc p > .desc7", {
-        duration: 0.5,
-        y: 0,
-        opacity: 1,
-      });
-    }
-  });
+  if (
+    scrollTop >
+    document.querySelector(".prof ").offsetTop + window.innerHeight * 1.5
+  ) {
+    gsap.to(".prof_tit .tit1", {
+      delay: 0.4,
+      duration: 0.4,
+      opacity: 1,
+      x: 0,
+    });
+    gsap.to(".prof_tit .tit2", {
+      delay: 0.5,
+      duration: 0.4,
+      opacity: 1,
+      x: 0,
+    });
+    gsap.to(".prof_tit .tit3", {
+      delay: 0.6,
+      duration: 0.4,
+      opacity: 1,
+      x: 0,
+    });
+    gsap.to(".prof_tit .tit4", {
+      delay: 0.7,
+      duration: 0.4,
+      opacity: 1,
+      x: 0,
+    });
+    gsap.to(".prof_img", { delay: 0.6, duration: 0.6, opacity: 1, y: 0 });
+    gsap.to(".prof_desc p > .desc1", {
+      delay: 0.8,
+      duration: 0.5,
+      y: 0,
+      opacity: 1,
+    });
+    gsap.to(".prof_desc p > .desc2", {
+      delay: 0.9,
+      duration: 0.5,
+      y: 0,
+      opacity: 1,
+    });
+    gsap.to(".prof_desc p > .desc3", {
+      delay: 1.0,
+      duration: 0.5,
+      y: 0,
+      opacity: 1,
+    });
+    gsap.to(".prof_desc p > .desc4", {
+      delay: 1.1,
+      duration: 0.5,
+      y: 0,
+      opacity: 1,
+    });
+    gsap.to(".prof_desc p > .desc5", {
+      delay: 1.2,
+      duration: 0.5,
+      y: 0,
+      opacity: 1,
+    });
+    gsap.to(".prof_desc p > .desc6", {
+      delay: 1.3,
+      duration: 0.5,
+      y: 0,
+      opacity: 1,
+    });
+    gsap.to(".prof_desc p > .desc7", {
+      delay: 1.3,
+      duration: 0.5,
+      y: 0,
+      opacity: 1,
+    });
+  }
 });
 
 //section3
@@ -343,18 +270,95 @@ window.addEventListener("scroll", () => {
       window.scrollY) + window.innerHeight;
 
   document.querySelector(".scroll").innerText = Math.round(scrollTop);
+
   // gsap.set(".prof_text span", { opacity: 0, x: -100 });
-  gsap.set(".skill_logo_img", { opacity: 0, x: -100 });
-  gsap.set(".skill_txt_tit", { opacity: 0, y: 70 });
-  gsap.set(".skill_txt_desc", { opacity: 0, y: 70 });
-
-  setTimeout(() => {
-    let tl = gsap.timeline();
-
-    tl.to(".skill_logo_img", { duration: 0.8, opacity: 1, x: 0 });
-    tl.to(".skill_txt_tit", { duration: 0.6, opacity: 1, y: 0 });
-    tl.to(".skill_txt_desc", { duration: 0.5, opacity: 1, y: 0 });
-  });
+  gsap.set(".skill_logo_img", { opacity: 0, x: 100 });
+  gsap.set(".skill_txt_tit p", { opacity: 0, y: 70 });
+  gsap.set(".skill_txt_desc p", { opacity: 0, y: 70 });
+  if (
+    scrollTop >
+    document.querySelector(".skill_logo").offsetTop + window.innerHeight * 0.5
+  ) {
+    gsap.to(".skill_logo_img", { delay: 0.3, duration: 0.8, opacity: 1, x: 0 });
+    gsap.to(".skill_txt_tit .tit1", {
+      delay: 0.8,
+      duration: 0.5,
+      opacity: 1,
+      y: 0,
+    });
+    gsap.to(".skill_txt_tit .tit2", {
+      delay: 0.9,
+      duration: 0.5,
+      opacity: 1,
+      y: 0,
+    });
+    gsap.to(".skill_txt_tit .tit3", {
+      delay: 1,
+      duration: 0.5,
+      opacity: 1,
+      y: 0,
+    });
+    gsap.to(".skill_txt_tit .tit4", {
+      delay: 1.2,
+      duration: 0.5,
+      opacity: 1,
+      y: 0,
+    });
+    gsap.to(".skill_txt_desc .desc1", {
+      delay: 0.8,
+      duration: 0.5,
+      opacity: 1,
+      y: 0,
+    });
+    gsap.to(".skill_txt_desc .desc2", {
+      delay: 0.9,
+      duration: 0.5,
+      opacity: 1,
+      y: 0,
+    });
+    gsap.to(".skill_txt_desc .desc3", {
+      delay: 1.0,
+      duration: 0.5,
+      opacity: 1,
+      y: 0,
+    });
+    gsap.to(".skill_txt_desc .desc4", {
+      delay: 1.1,
+      duration: 0.5,
+      opacity: 1,
+      y: 0,
+    });
+    gsap.to(".skill_txt_desc .desc5", {
+      delay: 1.2,
+      duration: 0.5,
+      opacity: 1,
+      y: 0,
+    });
+    gsap.to(".skill_txt_desc .desc6", {
+      delay: 1.3,
+      duration: 0.5,
+      opacity: 1,
+      y: 0,
+    });
+    gsap.to(".skill_txt_desc .desc7", {
+      delay: 1.4,
+      duration: 0.5,
+      opacity: 1,
+      y: 0,
+    });
+    gsap.to(".skill_txt_desc .desc8", {
+      delay: 1.5,
+      duration: 0.5,
+      opacity: 1,
+      y: 0,
+    });
+    gsap.to(".skill_txt_desc .desc9", {
+      delay: 1.6,
+      duration: 0.5,
+      opacity: 1,
+      y: 0,
+    });
+  }
 });
 
 // ease: "expo.out",
@@ -406,3 +410,76 @@ document
         button.textContent.trim().split("").join("</span><span>") +
         "</span></div>")
   );
+
+//SupahScroll
+class SupahScroll {
+  constructor(options) {
+    this.opt = options || {};
+    this.el = this.opt.el ? this.opt.el : ".supah_scroll";
+    this.speed = this.opt.speed ? this.opt.speed : 0.1;
+    this.init();
+  }
+
+  init() {
+    this.scrollY = 0;
+    this.supahScroll = document.querySelectorAll(this.el)[0];
+    this.supahScroll.classList.add("supah_scroll");
+    this.events();
+    this.update();
+    this.animate();
+  }
+
+  update() {
+    if (this.supahScroll === null) return;
+    document.body.style.height = `${
+      this.supahScroll.getBoundingClientRect().height
+    }px`;
+  }
+
+  pause() {
+    document.body.style.overflow = "hidden";
+    cancelAnimationFrame(this.raf);
+  }
+
+  play() {
+    document.body.style.overflow = "inherit";
+    this.raf = requestAnimationFrame(this.animate.bind(this));
+  }
+
+  destroy() {
+    this.supahScroll.classList.remove("supah_scroll");
+    this.supahScroll.style.transform = "none";
+    document.body.style.overflow = "inherit";
+    window.removeEventListener("resize", this.update);
+    cancelAnimationFrame(this.raf);
+    delete this.supahScroll;
+  }
+
+  animate() {
+    this.scrollY += (window.scrollY - this.scrollY) * this.speed;
+    this.supahScroll.style.transform = `translate3d(0,${-this.scrollY}px,0)`;
+    this.raf = requestAnimationFrame(this.animate.bind(this));
+  }
+
+  scrollTo(y) {
+    window.scrollTo(0, y);
+  }
+
+  staticScrollTo(y) {
+    cancelAnimationFrame(this.raf);
+    this.scrollY = y;
+    window.scrollTo(0, y);
+    this.supahScroll.style.transform = `translate3d(0,${-y}px,0)`;
+    this.play();
+  }
+
+  events() {
+    window.addEventListener("load", this.update.bind(this));
+    window.addEventListener("resize", this.update.bind(this));
+  }
+}
+//Initialize
+const supahscroll = new SupahScroll({
+  el: "#wrapper",
+  speed: 0.1,
+});
